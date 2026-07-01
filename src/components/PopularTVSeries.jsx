de3c2b3265b8386f32389fdcde2fe5fd53f725fd
Nariman <narimanhasanpanah@gmail.com>
2026-07-01 17:35:15 +0330

    Feat: add popular TV series section with tvserie cards

--- src/components/PopularTVSeries.jsx
+++ src/components/PopularTVSeries.jsx
@@ -1,5 +1,26 @@
+import { useContext } from "react";
+import { PopularMediaContext } from "../context/PopularMediaContext";
+import SerieCard from "./SerieCard";
+
 const PopularTVSeries = () => {
-  return <></>;
+  const { series } = useContext(PopularMediaContext);
+  return (
+    <>
+      <div id="popular-series" className="bg-white pb-10">
+        <div
+          id="popular-series-title"
+          className=" py-10 text-red-600 text-center pb-10 text-3xl md:text-4xl lg:text-5xl font-bold"
+        >
+          Popular TV Series
+        </div>
+        <div className="grid grid-cols-2 gap-5 px-3 justify-center sm:grid-cols-3 sm:px-5 md:px-8 md:grid-cols-4 lg:grid-cols-6 lg:px-28">
+          {series.map((serie) => (
+            <SerieCard key={serie.id} series={serie} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PopularTVSeries;
---
+++ src/components/SerieCard.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router";
+const SerieCard = ({ series }) => {
+  return (
+    <>
+      <Link to={`/tvseriesinfo/${series.id}`}>
+        <div
+          className="cursor-pointer hover:opacity-50 transition-opacity duration-300"
+          key={series.id}
+        >
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${series.poster_path}`}
+            className="rounded-3xl"
+            alt=""
+          />
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default SerieCard;
