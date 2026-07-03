01393082fa217bd67e5fb542b289fbc81f45cd38
Nariman <narimanhasanpanah@gmail.com>
2026-07-03 17:46:32 +0330

    Feat: create media trailer component & update movie card info

---
+++ src/components/MediaTrailer.jsx
@@ -0,0 +1,13 @@
+function MediaTrailer({ trailerKey }) {
+  return (
+    <div className="flex justify-center items-center py-10 px-3 bg-white">
+      <iframe
+        className="w-full rounded-lg md:w-2/3 md:px-0 aspect-video"
+        src={`https://www.youtube.com/embed/${trailerKey}`}
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default MediaTrailer;
--- src/pages/PopMovieCardInfo.jsx
+++ src/pages/PopMovieCardInfo.jsx
@@ -2,14 +2,18 @@ import { useParams } from "react-router";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import PopMovieInfoPage from "../components/PopMovieInfoPage";
+import MediaTrailer from "../components/MediaTrailer";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function PopMovieCardInfo() {
   const [movieInfo, setMovieInfo] = useState(null);
+  const [trailerKey, setTrailerKey] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
     const movieInfoURL = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+    const trailerKeyURL = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
     const options = {
       headers: {
         accept: "application/json",
@@ -17,10 +21,21 @@ function PopMovieCardInfo() {
       },
     };
 
-    axios
-      .get(movieInfoURL, options)
-      .then((movieInfoRes) => {
+    Promise.all([
+      axios.get(movieInfoURL, options),
+      axios.get(trailerKeyURL, options),
+    ])
+      .then(([movieInfoRes, trailerKeyRes]) => {
         setMovieInfo(movieInfoRes.data);
+
+        const trailer = trailerKeyRes.data.results.find(
+          (item) => item.type == "Trailer" && item.site == "YouTube",
+        );
+        const fallback = trailerKeyRes.data.results.find(
+          (item) => item.site == "YouTube",
+        );
+        if (trailer) setTrailerKey(trailer.key);
+        else if (fallback) setTrailerKey(fallback.key);
       })
       .catch((err) => console.error(err));
   }, [id]);
@@ -37,6 +52,11 @@ function PopMovieCardInfo() {
     <div className="bg-gray-300">
       <Navbar />
       <PopMovieInfoPage key={movieInfo.id} info={movieInfo} />
+      <div className=" text-red-500 bg bg-white text-center py-5 text-2xl md:text-3xl lg:text-4xl font-bold">
+        Official Trailer
+      </div>
+      <MediaTrailer trailerKey={trailerKey} />
+      <Footer />
     </div>
   );
 }
