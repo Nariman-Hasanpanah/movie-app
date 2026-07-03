bcf5f9f03ac7389161df8a02f0c3b3fcdf633e88
Nariman <narimanhasanpanah@gmail.com>
2026-07-03 19:00:11 +0330

    Feat: update tv serie card info

--- src/pages/PopSerieCardInfo.jsx
+++ src/pages/PopSerieCardInfo.jsx
@@ -2,14 +2,18 @@ import { useParams } from "react-router";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import PopSerieInfoPage from "../components/PopSerieInfoPage";
+import MediaTrailer from "../components/MediaTrailer";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function PopSerieCardInfo() {
   const [serieInfo, setSerieInfo] = useState(null);
+  const [trailerKey, setTrailerKey] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
     const serieInfoURL = `https://api.themoviedb.org/3/tv/${id}?language=en-US`;
+    const trailerKeyURL = `https://api.themoviedb.org/3/tv/${id}/videos?language=en-US`;
     const options = {
       headers: {
         accept: "application/json",
@@ -17,10 +21,21 @@ function PopSerieCardInfo() {
       },
     };
 
-    axios
-      .get(serieInfoURL, options)
-      .then((serieInfoRes) => {
+    Promise.all([
+      axios.get(serieInfoURL, options),
+      axios.get(trailerKeyURL, options),
+    ])
+      .then(([serieInfoRes, trailerKeyRes]) => {
         setSerieInfo(serieInfoRes.data);
+
+        const trailer = trailerKeyRes.data.results.find(
+          (item) => item.type == "Trailer" && item.site == "YouTube",
+        );
+        const fallback = trailerKeyRes.data.results.find(
+          (item) => item.site == "YouTube",
+        );
+        if (trailer) setTrailerKey(trailer.key);
+        else if (fallback) setTrailerKey(fallback.key);
       })
       .catch((err) => console.error(err));
   }, [id]);
@@ -34,9 +49,14 @@ function PopSerieCardInfo() {
   }
 
   return (
-    <div className="bg-gray-300">
+    <div className="bg-gray-300 dark:bg-gray-800">
       <Navbar />
       <PopSerieInfoPage key={serieInfo.id} info={serieInfo} />
+      <div className=" text-red-500 bg-white text-center py-5 text-2xl md:text-3xl lg:text-4xl font-bold">
+        Official Trailer
+      </div>
+      <MediaTrailer trailerKey={trailerKey} />
+      <Footer />
     </div>
   );
 }
